b0442e9fbb1a6ad3689004075da77f143433d512
Konstantin <kukoboris@gmail.com>
2021-01-16 00:02:28 +0300

    Correct some errors

--- script.js
+++ script.js
@@ -34,11 +34,11 @@ for (let i = 0; i < 2; i++) {
 
 appData.moneyPerDay = appData.budget / 30;
 
-alert('Ежедневный бюджет:' + appData.moneyPerDay);
+alert('Ежедневный бюджет составляет:' + appData.moneyPerDay + 'руб.');
 
 if (appData.moneyPerDay < 100) {
   console.log('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay > 2000) {
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
   console.log('Средний уровень достатка');
 } else if (appData.moneyPerDay > 2000) {
   console.log('Высокий уровень достатка');
