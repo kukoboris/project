cf4fd017380926d5cb8582f49c09584eeeec6a51
Konstantin <kukoboris@gmail.com>
2021-01-24 22:59:50 +0300

    Test

--- script.js
+++ script.js
@@ -45,19 +45,27 @@ function chooseExpences() {
 
 chooseExpences();
 
-appData.moneyPerDay = (appData.budget / 30).toFixed();
-
-alert('Ежедневный бюджет составляет:' + appData.moneyPerDay + 'руб.');
-
-if (appData.moneyPerDay < 100) {
-  console.log('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-  console.log('Средний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
-  console.log('Высокий уровень достатка');
-} else {
-  console.log('Упс...Произошла какая-то ошибка');
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed();
+  alert('Ежедневный бюджет составляет:' + appData.moneyPerDay + 'руб.');
 }
+
+detectDayBudget();
+
+// Расчет уровня достатка
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+  } else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
+  } else {
+    console.log('Упс...Произошла какая-то ошибка');
+  }
+}
+
+detectLevel();
 
 function checkSavings() {
   if (appData.savings == true) {
@@ -70,3 +78,26 @@ function checkSavings() {
 }
 
 checkSavings();
+
+// Функция для определения необязательных расходов
+
+function chooseOptExpenses() {              
+  for (let i = 1; i <= 3; i++) {
+    let a = prompt('Введите НЕобязательную статью расходов в этом месяце', '');
+
+    if (
+      typeof a === 'string' &&
+      typeof a != null &&
+      a !== '' &&
+      a.length < 50
+    ) {
+      console.log('done');
+      appData.optionalExpenses[i] = a;
+      console.log(appData.optionalExpenses);
+    } else {
+      console.log('Bad result');
+      i--;
+    }
+  }
+}
+chooseOptExpenses();
